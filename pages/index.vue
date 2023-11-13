630b203893dcbe9562feba47790d08f0f48dd8bc
ondrui <ondrui@yandex.ru>
2023-11-14 01:00:48 +0300

    -NEW- fixbugs: Hydration Errors table html structure - added tag <tbody>

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-const { data } = await useFetch('/api/tickers/?limit=10')
+const { data } = await useFetch("/api/tickers/?limit=10");
 </script>
 
 <template>
@@ -8,21 +8,25 @@ const { data } = await useFetch('/api/tickers/?limit=10')
 
     <table border="1 px solid">
       <thead>
-      <tr>
-        <th>Name</th>
-        <th>Symbol</th>
-        <th>Price</th>
-        <th>Details</th>
-      </tr>
+        <tr>
+          <th>Name</th>
+          <th>Symbol</th>
+          <th>Price</th>
+          <th>Details</th>
+        </tr>
       </thead>
-      <tr v-for="currency in data.data" :key="data.data.id">
-        <td>{{ currency.name }}</td>
-        <td>{{ currency.symbol }}</td>
-        <td>{{ currency.price_usd }}</td>
-        <td>
-          <NuxtLink :to="'/currency/' + currency.id">{{ currency.id }}</NuxtLink>
-        </td>
-      </tr>
+      <tbody>
+        <tr v-for="currency in data.data" :key="currency.id">
+          <td>{{ currency.name }}</td>
+          <td>{{ currency.symbol }}</td>
+          <td>{{ currency.price_usd }}</td>
+          <td>
+            <NuxtLink :to="'/currency/' + currency.id">{{
+              currency.id
+            }}</NuxtLink>
+          </td>
+        </tr>
+      </tbody>
     </table>
   </main>
 </template>
